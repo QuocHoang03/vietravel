7df57360d2202360a30032da6a1c05a2a3348789
QUOCHOANG22603\quoch <quochoang22603>
2023-03-30 20:49:56 +0700

    commit slider 20%

---
+++ src/components/Layout/components/Slider/data.js
@@ -0,0 +1,10 @@
+const dataNavs = [
+  { id: 1, name: 'Tour du lịch trọn gói', icon: 'faCarSide' },
+  { id: 2, name: 'Khách sạn', icon: require('/') },
+  { id: 3, name: 'Vé máy bay', icon: require('/') },
+  { id: 4, name: 'Combo  Vé máy bay + Khách sạn', icon: require('/') },
+  { id: 5, name: 'Combo Xe + Khách sạn', icon: require('/') },
+  { id: 6, name: 'Tra cứu Booking', icon: require('/') },
+];
+
+export { dataNavs };
--- src/components/Layout/components/Slider/index.js
+++ src/components/Layout/components/Slider/index.js
@@ -1,5 +1,37 @@
+import classNames from 'classnames/bind';
+import styles from './Slider.module.scss';
+import { dataNavs } from './data';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCarSide } from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
+
 function Slider() {
-  return <h2>Slider</h2>;
+  return (
+    <aside className={cx('slider')}>
+      {/* img slider */}
+      <div className={cx('grid')}>
+        <div className={cx('img-block')}>
+          <img className={cx('img')} src={require('./components/img/slider.jpg')} alt="slider"></img>
+        </div>
+      </div>
+      <div className={cx('grid', 'wide')}>
+        <div className={cx('col l-12')}>
+          <nav className={cx('nav')}>
+            {dataNavs.map((dataNav, index) => {
+              return (
+                <nav className={cx('nav-item')}>
+                  <div className={cx('nav-icon')}>
+                    <FontAwesomeIcon icon={dataNav.icon} />
+                  </div>
+                  <div className={cx('nav-name')}>{dataNav.name}</div>
+                </nav>
+              );
+            })}
+          </nav>
+        </div>
+      </div>
+    </aside>
+  );
 }
 
 export default Slider;
